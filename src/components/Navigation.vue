1d732b72ff31e54e64629b9da5393674d2e47bd7
Valentina Rubane <mail@getrawfish.com>
2020-03-21 21:51:55 +0100

    added new pages + nav design started + removed unsed img

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="nav">
-    
+    <nav class="nav__config">
+      <ul>
+        <li>
+          <router-link :to="{ name: 'config'}">
+            _config
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+    <nav class="nav__menu">
+      <ul>
+        <li>
+          <router-link :to="{ name: 'home'}">
+            _main
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'favs'}" class="nav__menu--favs">
+            <span>* *</span>
+            <span>****</span>
+            <span>***</span>
+            <span>**</span>
+            <span>*</span>
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'map'}">
+            _map
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'info'}">
+            _info
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Favs from '../views/Favs.vue'
+import Config from '../views/Config.vue'
+import Map from '../views/Map.vue'
+import Info from '../views/Info.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home'
   },
+  {
+    path: '/favorites',
+    name: 'favs'
+  },
+  {
+    path: '/configuration',
+    name: 'config'
+  },
+  {
+    path: '/map',
+    name: 'map'
+  },
+  {
+    path: '/information',
+    name: 'info'
+  }
 ]
 
 const router = new VueRouter({
