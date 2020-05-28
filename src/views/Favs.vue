8da879d7bdc85e0bbfe9860feaeb084a911c3314
Valentina Rubane <mail@getrawfish.com>
2020-05-28 22:43:25 +0200

    config view design

---
+++ src/views/Favs.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="favs">
+        <Navigation></Navigation>
+        <Days></Days>
+    </div>
+</template>
+
+<script lang="ts">
+    import Days from '@/components/Days.vue'
+
+    export default {
+        name: 'favs',
+        components: {
+            Days
+        }
+    }
+</script>
