db02d6ff85a93e7c4fab4f1c2d9f838800425132
Valentina Rubane <mail@getrawfish.com>
2020-06-21 18:54:02 +0200

    detailed schedule

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <Days></Days>
     <Navigation></Navigation>
   </div>
 </template>
 
 <script lang="ts">
   import Navigation from '@/components/Navigation.vue'
+  import Days from '@/components/Days.vue'
 
   export default {
     name: 'home',
     components: {
-      Navigation
+      Navigation,
+      Days
     }
   }
 </script>
